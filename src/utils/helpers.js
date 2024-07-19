241b79c88c2ff680105ae87908f6561cc18194cd
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-19 19:32:24 +0100

    helper function to format date

---
+++ src/utils/helpers.js
@@ -0,0 +1,10 @@
+// Helper function to format date and time
+export const formatDateTime = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${formattedHours}:${formattedMinutes} ${ampm}`;
+};
