1c0fcb8af36e1cdfdd96ac398048e3eea07ef7e4
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-20 14:19:41 +0100

    set staletime to 5mins

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300000, // 5 minutes in milliseconds
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
